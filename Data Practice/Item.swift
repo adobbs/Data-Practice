619a7041aef0f07e935b6e8bb825dbf572203fb2
Andy Dobbs <andrew.dobbs@gmail.com>
2024-01-06 18:57:46 -0500

    Initial Commit

---
+++ Data Practice/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Data Practice
+//
+//  Created by Andy Dobbs on 1/6/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
